0c0ba3ff7bf27e00ec805bf2a9d31c76ddd9d01c
Manan Shah <manans@stanford.edu>
2019-11-18 12:03:22 -0800

    Minor fixes to train.py, switch to perceptual loss

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from time import time
 
 import torch
 
-from models import SteganoGAN
+from models import *
 from critics import BasicCritic
 from decoders import DenseDecoder, BasicDecoder
 from encoders import BasicEncoder, DenseEncoder, ResidualEncoder
@@ -15,7 +15,7 @@ from loader import DataLoader
 
 def main():
     torch.manual_seed(42)
-    timestamp = int(time())
+    timestamp = str(int(time()))
 
     parser = argparse.ArgumentParser()
 
@@ -26,13 +26,13 @@ def main():
     parser.add_argument('--epochs', default=4, type=int)
 
     parser.add_argument('--data_depth', default=1, type=int)
-    parser.add_argument('--hidden_size', default=32, type=int)
+    parser.add_argument('--hidden_size', default=8, type=int)
     parser.add_argument('--dataset', default="div2k", type=str)
     parser.add_argument('--output', default=False, type=str)
     args = parser.parse_args()
 
-    train = DataLoader(os.path.join("data", args.dataset, "train"), shuffle=True)
-    validation = DataLoader(os.path.join("data", args.dataset, "val"), shuffle=False)
+    train = DataLoader(os.path.join("data", args.dataset, "train"), shuffle=True, num_workers=0)
+    validation = DataLoader(os.path.join("data", args.dataset, "val"), shuffle=False, num_workers=0)
 
     encoder = {
         "basic": BasicEncoder,
@@ -49,7 +49,7 @@ def main():
         "basic": BasicCritic
     }[args.critic]
 
-    model = SteganoGAN(
+    model = SteganoGANPerceptualLoss(
         data_depth=args.data_depth,
         encoder=encoder,
         decoder=decoder,
