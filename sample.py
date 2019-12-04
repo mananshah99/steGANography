346ce5691bce3aa7d26d4d32ce8e59195225eaef
Manan Shah <manan.shah.777@gmail.com>
2019-12-04 04:50:19 +0000

    Update sample.py to properly function with a large input dataset

--- sample.py
+++ sample.py
@@ -1,88 +1,61 @@
-# -*- coding: utf-8 -*-
-
 import argparse
+from matplotlib.image import imread, imsave
 from models import *
+import glob
+from tqdm import tqdm
+from scipy.misc import toimage
 
-def _get_steganogan(args):
-
+def _get_steganogan(path):
     steganogan_kwargs = {
-        'cuda': not args.cpu,
-        'verbose': args.verbose
+        'cuda': False,
+        'verbose': True,
+        'path': path
     }
-
-    if args.path:
-        steganogan_kwargs['path'] = args.path
-    else:
-        steganogan_kwargs['architecture'] = args.architecture
 
     return SteganoGAN.load(**steganogan_kwargs)
 
+def generate_samples(model, cover, n, size=1):
+    cover = torch.FloatTensor(cover) #.cuda()
+    h = list(cover.shape)[0]
+    w = list(cover.shape)[1]
 
-def _encode(args):
-    """Given loads a pretrained pickel, encodes the image with it."""
-    steganogan = _get_steganogan(args)
-    steganogan.encode(args.cover, args.output, args.message)
+    cover = cover.view(1, 3, h, w)
 
+    generated = []
+    for i in range(n):
+        payload_i = model._random_data(cover, data_size=size) #.cuda()
+        generated_i = model.encoder(cover, payload_i)[0].clamp(-1.0, 1.0)
+        generated.append((generated_i.view(h, w, 3).cpu().detach().numpy() + 1.0) * 127.5)
 
-def _decode(args):
-    try:
-        steganogan = _get_steganogan(args)
-        message = steganogan.decode(args.image)
-
-        if args.verbose:    
-            print('Message successfully decoded:')
-
-        print(message)
-
-    except Exception as e:
-        print('ERROR: {}'.format(e))
-
-
-def _get_parser():
-
-    # Parent Parser - Shared options
-    parent = argparse.ArgumentParser(add_help=False)
-    parent.add_argument('-v', '--verbose', action='store_true', help='Be verbose')
-    group = parent.add_mutually_exclusive_group()
-    group.add_argument('-a', '--architecture', default='dense',
-                       choices={'basic', 'dense', 'residual'},
-                       help='Model architecture. Use the same one for both encoding and decoding')
-
-    group.add_argument('-p', '--path', help='Load a pretrained model from a given path.')
-    parent.add_argument('--cpu', action='store_true',
-                        help='Force CPU usage even if CUDA is available')
-
-    parser = argparse.ArgumentParser(description='SteganoGAN Command Line Interface')
-
-    subparsers = parser.add_subparsers(title='action', help='Action to perform')
-    parser.set_defaults(action=None)
-
-    # Encode Parser
-    encode = subparsers.add_parser('encode', parents=[parent],
-                                   help='Hide a message into a steganographic image')
-    encode.set_defaults(action=_encode)
-    encode.add_argument('-o', '--output', default='output.png',
-                        help='Path and name to save the output image')
-    encode.add_argument('cover', help='Path to the image to use as cover')
-    encode.add_argument('message', help='Message to encode')
-
-    # Decode Parser
-    decode = subparsers.add_parser('decode', parents=[parent],
-                                   help='Read a message from a steganographic image')
-    decode.set_defaults(action=_decode)
-    decode.add_argument('image', help='Path to the image with the hidden message')
-
-    return parser
-
+    return generated
 
 def main():
-    parser = _get_parser()
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('-p', '--path')
+    parser.add_argument('-d', '--original_dataset')
+    parser.add_argument('-o', '--output_directory')
+    parser.add_argument('-n', '--num_cover_images', type=int)
+    parser.add_argument('-r', '--num_generated_per_cover', type=int)
+    parser.add_argument('-s', '--size', type=float)
+
     args = parser.parse_args()
 
-    if not args.action:
-        parser.print_help()
-        parser.exit()
+    model = _get_steganogan(args.path)
 
-    args.action(args)
+    # Generate samples
 
+    pbar = tqdm(total = args.num_cover_images * args.num_generated_per_cover)
+
+    num_processed = 0
+    for filename in glob.glob(args.original_dataset + '*'):
+        if num_processed > args.num_cover_images: break
+
+        samples = generate_samples(model, imread(filename, pilmode='RGB') / 127.5 - 1.0, args.num_generated_per_cover, size = args.size)
+        for i, sample in enumerate(samples):
+            toimage(sample.astype('uint8')).save(args.output_directory + '/' + filename.split('/')[-1].split('.')[0] + '_sample_' + str(i) + '.png')
+            pbar.update(1)
+        
+        num_processed += 1
+    
+    pbar.close()
 main()
